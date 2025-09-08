6a5db7dbc390562ca49697eda37caf75c1a856b2
addygit123 <minecraftaddy123@gmail.com>
2025-09-08 14:17:18 +0530

    feat: checkpoint - complete and working login flow

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 // import LandingPage from './pages/LandingPage';
-// import LoginPage from './pages/LoginPage';
+import LoginPage from './pages/LoginPage';
 import DashboardPage from './pages/DashboardPage';
+
 
 function App() {
   return (
     <Router>
       <Routes>
         {/* <Route path="/" element={<LandingPage />} /> */}
-        {/* <Route path="/login" element={<LoginPage />} /> */}
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
       </Routes>
     </Router>
